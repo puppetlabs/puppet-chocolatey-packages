269d3f4a9a0c97b283e99256d5274b797a8d901b
Glenn Sarti <glennsarti@users.noreply.github.com>
2018-04-17 10:22:35 +0800

    (WIN-134) Update for PDK 1.3.2.0 package
    This commit contains the changes to create a PDK 1.3.2.0 chocolatey package

--- automatic/pdk/tools/chocolateyinstall.ps1
+++ automatic/pdk/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'pdk'
 $url32       = ''
-$url64       = ''
+$url64       = 'https://downloads.puppetlabs.com/windows/pdk-1.3.2.0-x64.msi'
 $checksum32  = ''
-$checksum64  = ''
+$checksum64  = '45493f8950c339e085b4cea11059458c22c97add5c0dd57d1a15da2485f06312'
 
 $packageArgs = @{
   packageName    = $packageName
