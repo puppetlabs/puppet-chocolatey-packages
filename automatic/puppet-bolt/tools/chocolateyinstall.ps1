be53d1858737a797c308214ef4e06ad3684eac22
klab-sytems <klab-systems@outlook.com>
2024-04-18 14:46:24 -0400

    rebasing as requested

--- automatic/puppet-bolt/tools/chocolateyinstall.ps1
+++ automatic/puppet-bolt/tools/chocolateyinstall.ps1
@@ -1,26 +1,15 @@
 ﻿$packageName = 'puppet-bolt'
-$url32       = ''
-$url64       = 'https://downloads.puppet.com/windows/puppet-tools/puppet-bolt-3.29.0-x64.msi'
-$filename32  = ''
-$filename64  = 'puppet-bolt-3.29.0-x64_x64.msi'
-$checksum32  = ''
-$checksum64  = '8609EC21378198FD3A499D5E6F45F0E2057F25A59A28FA6140B7602520E4378B'
-
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64       = 'https://downloads.puppet.com/windows/puppet-tools/puppet-bolt-3.28.0-x64.msi'
+$checksum64  = 'E985094B52FA383F7D679FE5C1DAB870CBD63F07280CEAC13BC4C21F2B7198EC'
 
 $packageArgs = @{
-  packageName    = $packageName
-  fileType       = 'MSI'
-  silentArgs     = "/qn /norestart"
-  validExitCodes = @(0, 3010, 1641)
-}
-
-if ([string]::IsNullOrEmpty($filename32)) {
-  # If 64bit only, then only use the file parameter
-  $packageArgs['file'] = (Join-Path -Path $toolsDir -ChildPath $filename64)
-} else {
-  $packageArgs['file'] = (Join-Path -Path $toolsDir -ChildPath $filename32)
-  $packageArgs['file64'] = (Join-Path -Path $toolsDir -ChildPath $filename64)
+  packageName     = $packageName
+  fileType        = 'MSI'
+  url64bit        = $url64
+  checksum64      = $checksum64
+  checksumTypes64 = 'sha256'
+  silentArgs      = "/qn /norestart"
+  validExitCodes  = @(0, 3010, 1641)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
